60b9ecb2a4d83046dcf90b7687bc9fcb6b08e850
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2025-01-20 14:56:02 +0200

    update subgraph

--- subgraph/src/mappings.ts
+++ subgraph/src/mappings.ts
@@ -1,11 +1,19 @@
 import { Protobuf } from "as-proto/assembly";
-// import { Instructions as protoInstructions } from "./pb/sol/transactions/v1/Instructions";
+import { Reader } from "as-proto/assembly";
+import { Instructions } from "./pb/sol/transactions/v1/Instructions";
 import { MyEntity } from "../generated/schema";
 import { BigInt, log, crypto, Bytes} from "@graphprotocol/graph-ts";
 
 export function handleTriggers(bytes: Uint8Array): void {
-  const input = Protobuf.decode<protoInstructions>(bytes, protoInstructions.decode);
+  const input = Protobuf.decode<Instructions>(bytes, Instructions.decode);
   
+  input.instructions[0].
+
+  log.debug("Decoded input: {}", [input.instructions[0].claimBet.toString()]);
+
+
+  // log.debug("Decoded input: {}", [[input].toString()]);
+
   // No ID field has been found in the proto input...
   // The input has been hashed to create a unique ID, replace it with the field you want to use as ID
   const inputHash = crypto.keccak256(Bytes.fromUint8Array(bytes)).toHexString();
@@ -13,3 +21,4 @@ export function handleTriggers(bytes: Uint8Array): void {
 
   entity.save();
 }
+
