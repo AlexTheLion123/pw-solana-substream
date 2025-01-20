122ea24ac3ad77a9688ce100a5671f0a4208e3ea
Pure-lmao <ottertearsuac@gmail.com>
2025-01-20 15:44:22 +0000

    set entity values

--- subgraph/src/mappings.ts
+++ subgraph/src/mappings.ts
@@ -1,24 +1,76 @@
 import { Protobuf } from "as-proto/assembly";
 import { Reader } from "as-proto/assembly";
 import { Instructions } from "./pb/sol/transactions/v1/Instructions";
-import { MyEntity } from "../generated/schema";
+import { Bet } from "../generated/schema";
 import { BigInt, log, crypto, Bytes} from "@graphprotocol/graph-ts";
 
 export function handleTriggers(bytes: Uint8Array): void {
   const input = Protobuf.decode<Instructions>(bytes, Instructions.decode);
-  
-  input.instructions[0].
+  /*assumed ix is changed to 
+  ix = {
+    accounts[],
+    [instructionType]: {data},
+    programId,
+    timestamp,
+    txHash
+  }
+  */
+  input.instructions.forEach(ix => {
+    let accountAddr: String, entity: Bet
+    switch (true){
 
-  log.debug("Decoded input: {}", [input.instructions[0].claimBet.toString()]);
+      case ("placeBet" in ix || "placeFreeBet" in ix):
+        accountAddr = ix.accounts[2 + Number("placeFreeBet" in ix)] //is 2 in placeBet and 3 in placeFreeBet
+        entity = new Bet(accountAddr)
+        entity.account = accountAddr
+        entity.bettor = ix.accounts[0]
+        entity.placedAt = ix.timestamp //in unix seconds, might need to convert from ms to s. 
+        entity.minOdds = ix.placeBet.minOdds
+        entity.settledOdds = null
+        entity.amount = ix.placeBet.minOdds
+        entity.status = "PENDING"
+        entity.isLive = ix.placeBet.isLiveBet
+        entity.isSOLfree = ix.placeBet.isSolFree
+        entity.selections = ix.placeBet.selections
+        entity.betId = 0
+        entity.freeBetId = ix.placeBet.freeBetId //this might be 0 in non-free bet or it might error so set as null
+        entity.result = "PENDING"
+        entity.placed = {timestamp: ix.timestamp, txHash: ix.txHash}
+        entity.confirmed = null
+        entity.claimed = null
+        entity.save()
+        //break; //for some reason typescript doesnt like me adding breaks
+        
+      case("cancelBet" in ix):
+        accountAddr = ix.accounts[2]
+        entity = new Bet(accountAddr)
+        entity.status = "CANCELED"
+        entity.claimed = {timestamp: ix.timestamp, txHash: ix.txHash}
+        entity.save()
 
+        case("confirmBet" in ix):
+        accountAddr = ix.accounts[1]
+        entity = new Bet(accountAddr)
+        entity.status = ix.confirmBet.status
+        entity.betId = ix.confirmBet.betId
+        entity.confirmed = {timestamp: ix.timestamp, txHash: ix.txHash}
+        entity.save()
 
-  // log.debug("Decoded input: {}", [[input].toString()]);
+      case("claimBet" in ix):
+        accountAddr = ix.accounts[7]
+        entity = Bet.load(accountAddr)
+        const payout = ix.claimBet
+        if(payout > 0){
+          entity.settledOdds = payout / entity.amount
+          entity.result = (payout == entity.amount) ? "REFUND" : "WON"
+        }else{
+          entity.result = "LOST"
+        }
+        entity.status = "CLAIMED"
+        entity.claimed = {timestamp: ix.timestamp, txHash: ix.txHash}
+        entity.save()
+      }
+  })
 
-  // No ID field has been found in the proto input...
-  // The input has been hashed to create a unique ID, replace it with the field you want to use as ID
-  const inputHash = crypto.keccak256(Bytes.fromUint8Array(bytes)).toHexString();
-  let entity = new MyEntity(inputHash);
-
-  entity.save();
 }
 
